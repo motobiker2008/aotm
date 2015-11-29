35d09e63d72ea27532b80b3a0c1be9962edcec83
Vladimir Mikryukov <motobiker2008@gmail.com>
2015-11-29 11:23:21 +0300

    Implemented converters

---
+++ converter.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+__author__ = 'vladimir'
+import subprocess
+
+
+def wav_to_mp3(in_file, out_file, rate=44100, channels=2, bits=192):
+    bashCommand = "ffmpeg -i {in_file} -vn -ar {rate} -ac {channels} -ab {bits} -f mp3 {out_file}".format(
+        in_file=in_file, rate=rate, channels=channels, bits=bits, out_file=out_file)
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    #output = process.communicate()[0]
+
+
+def mp3_to_wav(in_file, out_file):
+    bashCommand = "ffmpeg -i {in_file} {out_file}".format(in_file=in_file, out_file=out_file)
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    #output = process.communicate()[0]
