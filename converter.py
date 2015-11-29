9b0670c1fb3ad544a5b62f32ccadea452ce6df5c
Vladimir Mikryukov <motobiker2008@gmail.com>
2015-11-29 11:31:42 +0300

    Tests for converters

--- converter.py
+++ converter.py
@@ -4,13 +4,18 @@ import subprocess
 
 
 def wav_to_mp3(in_file, out_file, rate=44100, channels=2, bits=192):
-    bashCommand = "ffmpeg -i {in_file} -vn -ar {rate} -ac {channels} -ab {bits} -f mp3 {out_file}".format(
+    bashCommand = "ffmpeg -y -i {in_file} -vn -ar {rate} -ac {channels} -ab {bits} -f mp3 {out_file}".format(
         in_file=in_file, rate=rate, channels=channels, bits=bits, out_file=out_file)
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    #output = process.communicate()[0]
+    output = process.communicate()[0]
 
 
 def mp3_to_wav(in_file, out_file):
-    bashCommand = "ffmpeg -i {in_file} {out_file}".format(in_file=in_file, out_file=out_file)
+    bashCommand = "ffmpeg -y -i {in_file} {out_file}".format(in_file=in_file, out_file=out_file)
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    #output = process.communicate()[0]
+    output = process.communicate()[0]
+
+
+if __name__ == '__main__':
+    wav_to_mp3('a.wav', 'a.mp3')
+    mp3_to_wav('a1.mp3', 'a1.wav')
