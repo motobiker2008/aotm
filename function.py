e6974efb6da8b048f7f728ec9a2bfd18f66ce5b1
diraliss <diraliss@ya.ru>
2015-12-19 17:52:08 +0300

    Renaming variables and moving all functions in one file

--- function.py
+++ function.py
@@ -1,20 +1,46 @@
 import matplotlib.pyplot as plt
 from scipy.fftpack import rfft
 
+'''
+From main it called
 
-def apl_freq(a, filename):
-    b = [(ele / 2 ** 8.) * 2 - 1 for ele in a]  # this is 8-bit track, b is now normalized on [-1,1)
-    c = rfft(b)  # calculate real part fourier transform (complex numbers list)
-    d = len(c)
-    plt.plot(abs(c[:(d - 1)]), 'r')
-    plt.savefig(filename+'apl_freq.png')
+from scipy.io import wavfile
+from function import *
+import numpy as np
+
+filename = 'chaikovsky30.wav'
+fs, data = wavfile.read(filename) # load the data
+signal = data.T[0] # this is a two channel soundtrack, I get the first track
+time = np.arange(len(data))/float(fs)
+
+ampl_freq(signal, time)
+ampl(signal, time)
+spectrum(signal)
+'''
+
+
+def ampl_freq(signal, time):
+    normal_signal = [(ele / 2 ** 8.) * 2 - 1 for ele in signal]  # this is 8-bit track, b is now normalized on [-1,1)
+    fft_result = rfft(normal_signal)  # calculate real part fourier transform (complex numbers list)
+
+    d = len(fft_result)/2
+    plt.plot(abs(fft_result[:(d - 1)]), 'r')
+    plt.savefig('apl_freq.png')
     plt.clf()
 
 
-def spectrum_freq(a, filename):
-    Pxx, freqs, t, plot = plt.specgram(
-        a,
+def spectrum(signal):
+    plt.specgram(
+        signal,
         Fs=44100,
         noverlap=int(128 * 0.5))
-    plt.savefig(filename+'spectrum.png')
+    plt.savefig('spectrum.png')
+    plt.clf()
+
+
+def ampl(signal, time):
+    a = len(signal)
+    b = len(time)
+    plt.plot(time[:(b - 1)], signal[:(a - 1)])
+    plt.savefig('amplitude.png')
     plt.clf()
