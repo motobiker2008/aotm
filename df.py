9594a29c7afdf74540415658ad8460fb96a7f983
diraliss <diraliss@ya.ru>
2015-12-21 20:06:16 +0300

    example use of Hann window

---
+++ df.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy.fft import fft, fftshift
+
+'''
+Пример работы окна Ханна
+'''
+
+window = np.hanning(51)
+plt.plot(window)
+plt.title("Hann window")
+plt.ylabel("Amplitude")
+plt.xlabel("Sample")
+plt.show()
+
+plt.figure()
+A = fft(window, 2048) / 25.5
+mag = np.abs(fftshift(A))
+freq = np.linspace(-0.5, 0.5, len(A))
+response = 20 * np.log10(mag)
+response = np.clip(response, -100, 100)
+plt.plot(freq, response)
+plt.title("Frequency response of the Hann window")
+plt.ylabel("Magnitude [dB]")
+plt.xlabel("Normalized frequency [cycles per sample]")
+plt.axis('tight')
+plt.show()
